10b905c8e6c3a7b49bef23af363b1a5c2845c2f2
Swapnil <swapnil.padaya2699@gmail.com>
2023-08-13 23:53:09 +0530

    Added todos

--- app/src/main/java/com/swapnil/copypast/repository/CopyPastRepository.kt
+++ app/src/main/java/com/swapnil/copypast/repository/CopyPastRepository.kt
@@ -5,6 +5,8 @@ import com.swapnil.copypast.model.ClipBoardItem
 import com.swapnil.copypast.repository.roomDB.utils.ClipBoardItemWrapper
 import javax.inject.Inject
 
+// TODO: create a interface and access clipboarditem via them.
+// TODO: Also create a View Model so that in mainactivity we can access this repo
 class CopyPastRepository @Inject constructor(private val clipBoardItemWrapper: ClipBoardItemWrapper) {
     private val TAG = "CopyPastRepository"
 //    private val _clipBoardItemList = MutableLiveData<List<ClipBoardItem>>()
