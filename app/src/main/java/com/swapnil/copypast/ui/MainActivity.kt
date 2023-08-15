d11a49dc823d1255c3745ac2748a11658f02660e
Swapnil <swapnil.padaya2699@gmail.com>
2023-08-15 19:31:15 +0530

    Added MainActivity with UI only

--- app/src/main/java/com/swapnil/copypast/ui/MainActivity.kt
+++ app/src/main/java/com/swapnil/copypast/ui/MainActivity.kt
@@ -2,64 +2,65 @@ package com.swapnil.copypast.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.swapnil.copypast.CopyPastApplication
 import com.swapnil.copypast.R
-import com.swapnil.copypast.model.ClipBoardItem
 import com.swapnil.copypast.repository.CopyPastDB
 import com.swapnil.copypast.repository.CopyPastRepository
 import com.swapnil.copypast.repository.dao.ClipBoardItemDao
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var copyPastDB: CopyPastDB
-
     @Inject
     lateinit var clipBoardItemDao: ClipBoardItemDao
-
     @Inject
     lateinit var copyPastRepository: CopyPastRepository
 
-    private lateinit var addButton: Button
-    private lateinit var getButton: Button
+    lateinit var tvCpStartMainText: TextView
+    lateinit var tvCpStartSubText: TextView
 
-    private lateinit var text: TextView
+    lateinit var tvCpCounterMainText: TextView
+    lateinit var tvCpCounterSubText: TextView
 
-    lateinit var scope: CoroutineScope
+    lateinit var tvCpPermissionMainText: TextView
+    lateinit var tvCpPermissionSubText: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         (application as CopyPastApplication).applicationComponent.inject(this)
-
-        addButton = findViewById(R.id.btn_add)
-        getButton = findViewById(R.id.btn_get)
-        text = findViewById(R.id.txt_display)
-        addButton.setOnClickListener(addButtonListener())
-        getButton.setOnClickListener(getButtonListener())
-        scope = CoroutineScope(Dispatchers.IO)
+        initViews()
+        setViewText()
     }
 
-    private fun addButtonListener() = View.OnClickListener {
-        scope.launch {
-            copyPastRepository.addClipBoardItem(ClipBoardItem())
-        }
+    private fun initViews() {
+        val includedStartCp = findViewById<LinearLayout>(R.id.included_starting_cp)
+        tvCpStartMainText = includedStartCp.findViewById(R.id.tv_main_text)
+        tvCpStartSubText = includedStartCp.findViewById(R.id.tv_sub_text)
+
+        val includedCounterCp = findViewById<LinearLayout>(R.id.included_counter_cp)
+        tvCpCounterMainText = includedCounterCp.findViewById(R.id.tv_main_text)
+        tvCpCounterSubText = includedCounterCp.findViewById(R.id.tv_sub_text)
+
+        val includedPermissionCp = findViewById<LinearLayout>(R.id.included_permission_cp)
+        tvCpPermissionMainText = includedPermissionCp.findViewById(R.id.tv_main_text)
+        tvCpPermissionSubText = includedPermissionCp.findViewById(R.id.tv_sub_text)
     }
 
-    private fun getButtonListener() = View.OnClickListener {
-        scope.launch {
-            val list = copyPastRepository.getClipBoardListToBeShown()
-            runOnUiThread {
-                text.text = list.joinToString { x -> x.id.toString()  + " -- ${x.text} \n\n" }
-            }
-        }
+    private fun setViewText() {
+        tvCpStartMainText.text = getString(R.string.start_copypast)
+        tvCpStartSubText.text = getString(R.string.start_copypast_sub)
+
+        tvCpCounterMainText.text = getString(R.string.counter_copypast)
+        tvCpCounterSubText.text = getString(R.string.counter_copypast_sub)
+
+        tvCpPermissionMainText.text = getString(R.string.permission_copypast)
+        tvCpPermissionSubText.text = getString(R.string.permission_copypast_sub_off)
     }
 }
 
